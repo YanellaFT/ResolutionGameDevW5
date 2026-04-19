4441a016d1be7c6b1991191761c384cc5d31dfdb
yanellaft <yanellaft@gmail.com>
2026-04-19 15:02:35 -0400

    added API

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const API_ENDPOINT = 'https://icanhazdadjoke.com/';
+const XHR = new XMLHttpRequest();
 
+function  getJoke() {
+    XHR.open('GET', API_ENDPOINT);
+
+    XHR.setRequestHeader('Accept', 'application/json');
+    XHR.responseType = 'json';
+
+    XHR.onload = function() {
+        showJoke(XHR.response.joke);
+        setButtonCta(false);
+    }
+    
+    XHR.onerror = function() {
+        showError("An error occurec, please try again");
+        setButtonCta(false);
+    }
+
+    XHR.send();
+
+}
